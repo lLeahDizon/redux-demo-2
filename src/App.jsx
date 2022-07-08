18dcc4f6e1e9ea04b48a3e925271d2970e046f57
lLeahDizon <516330797@qq.com>
2022-07-08 23:16:29 +0800

    feat: 添加 log

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,18 @@ export default () => {
     </appContext.Provider>
   )
 }
-const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier/></section>
-const 三儿子 = () => <section>三儿子</section>
+const 大儿子 = () => {
+  console.log('大儿子执行了 ' + Math.random())
+  return <section>大儿子<User/></section>
+}
+const 二儿子 = () => {
+  console.log('二儿子执行了 ' + Math.random())
+  return <section>二儿子<UserModifier/></section>
+}
+const 三儿子 = () => {
+  console.log('三儿子执行了 ' + Math.random())
+  return <section>三儿子</section>
+}
 const User = () => {
   const {appState} = useContext(appContext)
   return <div>User:{appState.user.name}</div>
